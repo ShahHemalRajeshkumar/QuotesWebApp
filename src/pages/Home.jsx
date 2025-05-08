61d3bbfbb4c15808b056228c0f4f47ffb07222f1
ShahHemalRajeshkumar <shahhemal2454@gmail.com>
2025-05-08 23:19:46 +0530

    updated home file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import QuoteTable from "../components/QuoteTable";
 import Pagination from "../components/Pagination";
 import RandomQuote from "../components/RandomQuote";
@@ -7,16 +7,16 @@ const Home = () => {
   const [quotes, setQuotes] = useState([]);
   const [page, setPage] = useState(1);
 
-  const fetchQuotes = async () => {
+  const fetchQuotes = useCallback(async () => {
     const skip = (page - 1) * 10;
     const res = await fetch(`${process.env.REACT_APP_QUOTES_API}?limit=10&skip=${skip}`);
     const data = await res.json();
-    setQuotes(data.quotes); 
-  };
+    setQuotes(data.quotes);
+  }, [page]);
 
   useEffect(() => {
     fetchQuotes();
-  }, [page, fetchQuotes]);
+  }, [fetchQuotes]);
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-100 to-cyan-50 dark:from-indigo-900 dark:to-purple-900 p-4 sm:p-6 lg:p-8">
